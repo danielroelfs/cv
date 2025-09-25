214995f6c8db2c8756651e6010d4878f642abfe2
Daniel Roelfs <daniel@roelfs.net>
2025-09-25 12:43:41 +0200

    add flag

--- render_cv.sh
+++ render_cv.sh
@@ -7,7 +7,8 @@ quarto render cv.qmd -P pdf_mode:true --metadata format.html.embed-resources=tru
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
     --headless \
     --virtual-time-budget=100000000 \
-    --print-to-pdf=roelfs_cv.pdf \
+    --allow-file-access-from-files \
+    --print-to-pdf=cv.pdf \
     tmp.html
 
-rm tmp.html
+#rm tmp.html
