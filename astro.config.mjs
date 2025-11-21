b810b03315abfb3dd0931fba6032fac38a7f2d70
Daniel Roelfs <daniel@roelfs.net>
2025-11-21 21:56:52 +0100

    convert to astro

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import yaml from '@rollup/plugin-yaml';
+
+export default defineConfig({
+    vite: {
+        plugins: [yaml()]
+    }
+});
--- render_cv.sh
+++ render_cv.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-quarto render cv.qmd -P pdf_mode:false --metadata format.html.embed-resources=false -o index.html
+npm run build    
 
-quarto render cv.qmd -P pdf_mode:true --metadata format.html.embed-resources=true -o tmp.html
-    
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
     --headless \
-    --virtual-time-budget=100000000 \
-    --allow-file-access-from-files \
-    --print-to-pdf=cv.pdf \
-    tmp.html
-
-#rm tmp.html
+    --no-pdf-header-footer \
+    --print-to-pdf=dist/roelfs_cv.pdf \
+    dist/index.html
