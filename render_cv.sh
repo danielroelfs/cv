491f23cc505ba1c734c461db4b10779e8309909d
Daniel Roelfs <daniel@roelfs.net>
2025-11-22 18:17:01 +0100

    add settings

--- render_cv.sh
+++ render_cv.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-EMBED_RESOURCES=false quarto render cv.qmd -P pdf_mode:false -o index.html
+quarto render cv.qmd -P pdf_mode:false -o index.html
 
-EMBED_RESOURCES=true quarto render cv.qmd -P pdf_mode:true -o tmp.html
+quarto render cv.qmd -P pdf_mode:true -o tmp.html
 
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
     --headless \
     --virtual-time-budget=600000000 \
+    --allow-file-access-from-files \
+    --disable-web-security \
+    --run-all-compositor-stages-before-draw \
     --print-to-pdf=roelfs_cv.pdf \
     tmp.html
 
