0c408408bab67dbd955b8d68b73772ab8e4421d2
Daniel Roelfs <daniel@roelfs.net>
2025-11-22 17:26:28 +0100

    working pdf

--- render_cv.sh
+++ render_cv.sh
@@ -6,8 +6,8 @@ quarto render cv.qmd -P pdf_mode:true -o tmp.html
 
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
     --headless \
-    --virtual-time-budget=20000000 \
-    --print-to-pdf=cv.pdf \
+    --virtual-time-budget=600000000 \
+    --print-to-pdf=roelfs_cv.pdf \
     tmp.html
 
 rm tmp.html
